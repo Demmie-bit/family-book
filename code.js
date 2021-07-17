4435b24ce7ff40865b043694b510b2ae02d0b531
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2021-07-16 22:08:30 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,12 +1,16 @@
+var names = ["Bikash Paul - Dad ","Anjana paul - Mom", " Anwesha Paul - me/ Big sister", "Aaryav Paul - Little brother"];
+console.log(names);
+var img = ["https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg","https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg", " https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg", "https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg"];
+console.log(names);
 var i = 0;
 function update(){
-    i++;
-    var number = 5
+    var number = 3
     if(i > number){
         i = 0
     }
-    var update_img = images[i];
+    var update_img = img[i];
     var update_Name = names[i];
-    document.getElementById("names").src = update_img;
-    document.getElementById("images").innerHTML = update_Name;
+    document.getElementById("img").src = update_img;
+    document.getElementById("names").innerHTML = update_Name;
+    i++;
 };
