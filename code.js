3f82b350c8fff4e8e0d5e648eee75a4174510621
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2021-07-17 10:18:46 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var names = ["Bikash Paul - Dad ","Anjana paul - Mom", " Anwesha Paul - me/ Big sister", "Aaryav Paul - Little brother"];
 console.log(names);
-var img = ["https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg","https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg", " https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg", "https://i.ytimg.com/vi/s_I4tqUMwY0/maxresdefault.jpg"];
+var img = ["dad-memes-funny-03-720x691.jpg","mothers-day-memes-2020-1.jpg", "birthday-quotes-for-sister-funny-1_result.jpg", "when-you-slap-your-little-brother-and-he-starts-to-36561290.png"];
 console.log(names);
 var i = 0;
 function update(){
